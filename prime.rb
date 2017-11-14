583b2c8613e5e94273fe32f1d2da1f3523eff69a
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-14 22:31:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+
+  if num <= 1
+    return false
+  else
+    arr = Array (2...num)
+    arr.each do |n|
+      if num % n == 0
+        return false
+      end
+    end
+    return true
+  end
+end
