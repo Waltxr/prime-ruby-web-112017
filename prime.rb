1e30039b262f19f9316a9f290820fc9973b149f9
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-14 22:16:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
 # Add  code here!
 def prime?(num)
-  arr = Array (2...num)
+  
+  Array (2...num.abs)
 
   arr.each do |n|
-    if num % n == 0
+    if num.abs % n == 0
       return false
     end
     return true
