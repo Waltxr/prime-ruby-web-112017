4c9cf99a21e970b5a9b79c1220ab42f9cefef0f5
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-14 22:25:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,14 +3,13 @@ def prime?(num)
 
   if num <= 1
     return false
-  end
-
-  arr = Array (2...num.abs)
-
-  arr.each do |n|
-    if num % n == 0
-      return false
+  else
+    arr = Array (2...num.abs)
+    arr.each do |n|
+      if num % n == 0
+        return false
+      end
+      return true
     end
-    return true
   end
 end
